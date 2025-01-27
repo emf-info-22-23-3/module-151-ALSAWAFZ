a353174ad98913d316e2a8abd2682b9eb145c210
zeina <zeina.alsawaf@studentfr.ch>
2025-01-27 14:53:05 +0100

    m

---
+++ src/exercices/exercice6/equipes.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+  <header>
+    <link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
+</header>
+  <body>
+    <div id="conteneur">
+      <h1>Les équipes de National League</h1>    
+      <table border= "1">
+      <tr>
+        <td>ID</td>
+        <td>Club</td>
+      </tr>
+      <?php
+        require('ctrl.php');
+        $equipes = getEquipes();
+        $id = 1;
+
+        foreach ($equipes as $equip) {
+        echo "<tr>";
+        echo"<td>" . $id. "</td>";
+        echo"<td>" . $equip. "</td>";
+        echo "</tr>";
+        $id++;
+        }
+      ?>
+      </table>
+    </div>
+  </body>
+</html>
