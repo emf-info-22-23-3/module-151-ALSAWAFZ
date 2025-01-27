ea51ebc6e05113b8302ce3eea4ad32c168484be3
zeina <zeina.alsawaf@studentfr.ch>
2025-01-27 16:06:09 +0100

    q

--- src/exercices/exercice6/ctrl.php
+++ src/exercices/exercice6/ctrl.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 include_once('wrk/Wrk.php');
 include_once('beans/Equipe.php');
 
@@ -10,7 +10,7 @@ $equipes = $wrk->getEquipes();
 foreach ($equipes as $index => $equip) {
     echo 'ID: ' . ($index + 1) . ', Club: ' . $equip->getName() . '<br>';
 }
-*/
+
 
 include_once('beans/Membre.php');
 $membre = new Membre('Paul', 25);
