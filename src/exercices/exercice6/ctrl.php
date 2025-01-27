070b362ac28f6505ffba94aad8f12142ff205d15
zeina <zeina.alsawaf@studentfr.ch>
2025-01-27 15:42:12 +0100

    o

--- src/exercices/exercice6/ctrl.php
+++ src/exercices/exercice6/ctrl.php
@@ -4,3 +4,14 @@ function getEquipes(){
   return getEquipesFromDB();
 }
 ?>
+
+<?php
+
+include_once('Membre.php');
+
+$membre = new Membre('Paul', 25); 
+$nom = $membre->getNom();
+$numero = $membre->numero;
+
+echo 'Un nouveau membre! Nom: ' . $nom . ', son âge: ' . $numero . '.';
+?>
