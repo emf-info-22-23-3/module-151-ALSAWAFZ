d1ded246991cef4fce7bae8bc93fece26cc6584c
zeina <zeina.alsawaf@studentfr.ch>
2025-01-27 13:52:49 +0100

    a

---
+++ src/exercices/exercice4/client/server.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type: application/xml"); 
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<equipes>';
+echo '<equipe><id>3</id><nom>SC Bern</nom></equipe>';
+echo '<equipe><id>4</id><nom>Fribourg-Gottéron</nom></equipe>';
+echo '<equipe><id>7</id><nom>HC Davos</nom></equipe>';
+echo '<equipe><id>8</id><nom>Genève-Servette</nom></equipe>';
+echo '</equipes>';
+?>
