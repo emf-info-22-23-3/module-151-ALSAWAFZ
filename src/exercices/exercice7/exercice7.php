529af9873d7b1082aaa9511a32510b19e2162218
zeina <zeina.alsawaf@studentfr.ch>
2025-01-28 10:55:33 +0100

    w

--- src/exercices/exercice7/exercice7.php
+++ src/exercices/exercice7/exercice7.php
@@ -1,13 +1,15 @@
 <?php
-	$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
-	$reponse = $bdd->query('select pk_jeux_video, titre from jeux_video');
-	
-	
-	while ($row = $reponse->fetch())
-	{
-	
-		echo $row['titre'] . $row['pk_jeux_video'];
-	
-	}
-	$reponse->closeCursor();
+$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
+$reponse = $bdd->query('select pk_jeux_video, titre from jeux_video');
+
+
+while ($row = $response->fetch())
+{
+
+	echo $row['titre'] . $row['pk_jeux_video'];
+
+
+}
+$reponse->closeCursor();
 ?>
+
