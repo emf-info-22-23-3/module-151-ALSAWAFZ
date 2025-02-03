5b3df26583f9ad77306017eb559b3cb3d7e69742
zeina <zeina.alsawaf@studentfr.ch>
2025-02-03 13:57:00 +0100

    tt

--- src/exercices/exercice9/serveur/serveur.php
+++ src/exercices/exercice9/serveur/serveur.php
@@ -1,11 +1,12 @@
 <?php
-
+session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	if($_POST['action'] == "connect")
 	{
 		
 		if ($_POST['password'] == 'emf') {
-            $_SESSION['logged'] = 'emf';
+			
+            $_SESSION['logged'] = true;
             echo '<result>true</result>';
         } else {
             unset($_SESSION['logged']);
@@ -15,15 +16,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 	if($_POST['action'] == "disconnect")
 	{
-		unset($_SESSION['logged']);
+		session_destroy();
+		session_unset();
         echo '<result>true</result>';
 	}
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET'){
 	if($_GET['action'] == "getInfos")
-	{
-		if (isset($_SESSION['logged']) && $_SESSION['logged'] == 'emf') {
+		{
+		if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
             echo '<users>';
             echo '<user><name>Victor Legros</name><salaire>9876</salaire></user>';
             echo '<user><name>Marinette Lachance</name><salaire>7540</salaire></user>';
@@ -31,8 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET'){
             echo '<user><name>Basile Ledisciple</name><salaire>2384</salaire></user>';
             echo '</users>';
         } else {
-            echo '<message>DROITS INSUFFISANTS</message>';
         }
+		if($_GET['action'] == "disconnect")
+	{
+		unset($_SESSION['logged']);
+        echo '<result>true</result>';
+	}
 	}
 }
 
