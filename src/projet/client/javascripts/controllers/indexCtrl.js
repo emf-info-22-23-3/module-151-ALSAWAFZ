cf8556a8f6b1c8451ebf4dab29ed03b03b2eefb5
zeina <zeina.alsawaf@studentfr.ch>
2025-02-17 09:22:46 +0100

    zk

--- src/projet/client/javascripts/controllers/indexCtrl.js
+++ src/projet/client/javascripts/controllers/indexCtrl.js
@@ -1,20 +1,10 @@
 
 function chargerMatchsSuccess(data, text, jqXHR)
 {   
-	var cmbMatchs = document.getElementById("cmbMatchs");
-    $(data).find("matchs").each(function() {
-      var matchs = new Matchs();
-      matchs.setPk($(this).find("pk_matchs").text());
-      matchs.setSpiel($(this).find("spiel").text());
-      matchs.setWochentag($(this).find("wochentag").text());
-      matchs.setDatum($(this).find("datum").text());
-      matchs.setMatchZeit($(this).find("matchZeit").text());
-      matchs.setFK_Enemy_Team($(this).find("fk_Enemy_Team").text());
-      matchs.setHalle($(this).find("halle").text());
-
-    
-	  cmbMatchs.options[cmbMatchs.options.length] = new Option(matchs, JSON.stringify(matchs));
-    });  
+    $.each(data, function(index, matchs) {
+      var row = '<tr><td>' + matchs.id + '</td><td>' + matchs.spiel + '</td></tr>';
+      $('#match-plan tbody').append(row);
+  });
 }
 
 function chargerMatchsError(request, status, error) {
@@ -23,23 +13,12 @@ function chargerMatchsError(request, status, error) {
 
 
 $(document).ready(function() {
-  var cmbMatchs = $("#cmbMatchs");
-  var matchs = '';
-
   $.getScript("javascripts/helpers/dateHelper.js", function() {
     console.log("dateHelper.js chargé !");
   });
-  $.getScript("javascripts/beans/matchs.js", function() {
-    console.log("matchs.js chargé !");
-  });
   $.getScript("javascripts/services/servicesHttp.js", function() {
     console.log("servicesHttp.js chargé !");
-    chargerPays(chargerMatchsSuccess, chargerMatchsError);
+    chargerMatchs(chargerMatchsSuccess, chargerMatchsError);
   });
-  cmbPays.change(function(event) {
-    matchs = this.options[this.selectedIndex].value;
-    chargerSkieurs(JSON.parse(pays).pk, chargerSkieursSuccess, chargerSkieursError);
-  });
-
 });
 
