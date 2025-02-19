8333f7a853eef5634fa2ea88d5927fa2d7f5271f
ZeinaAlsawaf <zeina.alsawaf@studentfr.ch>
2025-02-19 22:28:01 +0100

    ,kmbl

--- src/projet/finalTvMurten.sql
+++ src/projet/finalTvMurten.sql
@@ -13,7 +13,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 
 -- -----------------------------------------------------
 -- Schema DB_finalTVMurten
--- -----------------------------------------------------
+-- ------------------------j-----------------------------
 CREATE SCHEMA IF NOT EXISTS `DB_finalTVMurten` DEFAULT CHARACTER SET utf8 ;
 USE `DB_finalTVMurten` ;
 
